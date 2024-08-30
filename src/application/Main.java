bfbb99b0cd1e35594de4560659cd8e16b5bc928b
Diegorrech <diegorrech@gmail.com>
2024-08-29 21:25:00 -0300

    primeiro commit

---
+++ src/application/Main.java
@@ -0,0 +1,41 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Student;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		
+		Student  student = new Student();
+		
+		
+		System.out.println(" digite uma nota ");
+		
+		student.name = sc.nextLine();
+		student.grade1 = sc.nextDouble();
+		student.grade2 = sc.nextDouble();
+		student.grade3 = sc.nextDouble();
+		
+		
+		System.out.printf("final grade : %.2f%n " , student.finalGrade());
+		
+		if (student.finalGrade() < 60.0) {
+			System.out.println(" failed");
+			System.out.printf(" missing %.2f point%n" , student.missinPoints());
+		}
+		else {
+			System.out.println(" pass");
+		}
+		
+		
+		sc.close();
+	}
+
+}
